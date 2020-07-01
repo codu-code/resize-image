f9c0d3ccc7eb4c09f5f72e4c634e3e51027daf3b
Niall Maher <niall@codu.ie>
2020-07-01 16:55:02 +0100

    Add completed code

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 function process() {
-  console.log("I'm connected");
+  const file = document.querySelector("#upload").files[0];
+
+  if (!file) return;
+
+  const reader = new FileReader();
+
+  reader.readAsDataURL(file);
+
+  reader.onload = function (event) {
+    const imgElement = document.createElement("img");
+    imgElement.src = event.target.result;
+    document.querySelector("#input").src = event.target.result;
+
+    imgElement.onload = function (e) {
+      const canvas = document.createElement("canvas");
+      const MAX_WIDTH = 400;
+
+      const scaleSize = MAX_WIDTH / e.target.width;
+      canvas.width = MAX_WIDTH;
+      canvas.height = e.target.height * scaleSize;
+
+      const ctx = canvas.getContext("2d");
+
+      ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height);
+
+      const srcEncoded = ctx.canvas.toDataURL(e.target, "image/jpeg");
+
+      // you can send srcEncoded to the server
+      document.querySelector("#output").src = srcEncoded;
+    };
+  };
 }
